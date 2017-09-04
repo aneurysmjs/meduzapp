e30ed8a8666dc14328a264b55cc6dbef3b93792c
aneurysm <aneurysmjs@outlook.com>
2017-09-04 15:07:31 +0300

    feat(components): add `MFooter` component
    apply Vue's `name slots` functionality to put other elements where the footer's columns are, in this
    case there are 3 of them

--- src/app/App.vue
+++ src/app/App.vue
@@ -2,16 +2,23 @@
   <div id="app">
     <m-header :brand-name="brandName"></m-header>
     <router-view></router-view>
+    <m-footer>
+      <h2 class="text-center" slot="col1">Col1</h2>
+      <h2 class="text-center" slot="col2">Col2</h2>
+      <h2 class="text-center" slot="col3">Col3</h2>
+    </m-footer>
   </div>
 </template>
 
 <script>
   import MHeader from 'components/MHeader/MHeader.vue';
+  import MFooter from 'components/MFooter/MFooter.vue';
 
   export default {
     name: 'app',
     components: {
-      MHeader
+      MHeader,
+      MFooter
     },
     data () {
       return {
---
+++ src/components/MFooter/MFooter.vue
@@ -0,0 +1,41 @@
+<template>
+  <footer class="my-3">
+    <h4 class="text-center">
+      Subscribe
+    </h4>
+    <div class="row">
+      <div class="col" v-for="col in columns">
+        <slot :name="col.name"></slot>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+
+  export default {
+    data () {
+      return {
+        columns: [
+          {
+            name: 'col1'
+          },
+          {
+            name: 'col2'
+          },
+          {
+            name: 'col3'
+          }
+        ]
+      }
+    }
+  }
+
+</script>
+
+
+<style lang="scss" scoped>
+  footer {
+    background: #fafafa;
+  }
+</style>
