fe2e3e28132a09ba1bfd84084319d14f75a8b0a3
aneurysm <aneurysmjs@outlook.com>
2017-09-05 03:04:25 +0300

    feat(MHome): get records per page when clickin on `load more` button
    add `MButton` component which triggers any callback passed to it
    
    solves #3

--- src/components/MHome/MHome.vue
+++ src/components/MHome/MHome.vue
@@ -1,6 +1,13 @@
 <template>
   <section>
     <m-news-feed :news="news"></m-news-feed>
+    <div class="text-center">
+      <m-button
+        text="Load more"
+        :is-blocked="isButtonBlocked"
+        @on-click="loadMoreHandler">
+      </m-button>
+    </div>
     <m-error-logger v-if="isError" :message="isError.message"></m-error-logger>
   </section>
 </template>
@@ -8,19 +15,35 @@
   import api from 'api';
   import MErrorLogger from 'shared/components/MErrorLogger/MErrorLogger';
   import MNewsFeed from 'shared/components/MNewsFeed/MNewsFeed';
+  import MButton from 'shared/components/MButton/MButton';
 
   export default {
     data () {
       return {
-        news: {
-          type: Object
-        },
-        isError: undefined
+        news: {},
+        isError: undefined,
+        /**
+         * @private
+         *
+         * Number of records per page
+         */
+        _records: 5,
+        /**
+         * @private
+         *
+         * How many records are left for each request, so it can be set a limit
+         */
+        _limit: 0,
+        /**
+         * @type {Boolean}
+         */
+        isButtonBlocked: false
       }
     },
     components: {
       MErrorLogger,
-      MNewsFeed
+      MNewsFeed,
+      MButton
     },
     created () {
       /**
@@ -32,17 +55,40 @@
       /**
        * get the news feed from `https://meduza.io`
        *
-       * return {Promise<any>}
+       * @return {Promise<any>}
        */
       async getNews () {
-        let url = `search?chrono=news&locale=ru&page=0&per_page=24`;
+
+        let url = `search?chrono=news&locale=ru&page=0&per_page=${this._records}`;
+
         try {
+          // destructure response's `data` property
           let { data } = await api.get(url);
-          this.news = data.documents;
+          // destructure data's `_count` and `documents` properties.
+          let { _count, documents } = data;
+          // set the record's limit
+          this._limit = _count;
+          // clone the documents in a new object.
+          this.news = Object.assign({}, documents);
           // clean error in case request is successful.
           this.isError = undefined;
         } catch(err) {
+          // set the current error to whatever the response throws.
           this.isError = err;
+        }
+
+      },
+      /**
+       * Gets the more records from the meduza's API.
+       *
+       * @return {void}
+       */
+      loadMoreHandler () {
+        if (this._records < this._limit) { // get 5 records if it less than the limit.
+          this._records += 5;
+          this.getNews();
+        } else { // otherwise block the button.
+          this.isButtonBlocked = true;
         }
       }
     }
---
+++ src/shared/components/MButton/MButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <button type="button"
+          class="btn"
+          :disabled="isBlocked"
+          @click="click">
+    {{ text }}
+  </button>
+</template>
+<script>
+  export default {
+    props: {
+      text: {
+        type: String,
+        required: true
+      },
+      isBlocked: {
+        type: Boolean
+      }
+    },
+    methods: {
+      click () {
+        this.$emit('on-click');
+      }
+    }
+  }
+</script>
+<style lang="scss" scoped>
+  button.btn {
+    background: #24d1c4 !important;
+    color: #ffffff;
+    cursor: pointer;
+    &:hover {
+      background: #25c5b9 !important;
+    }
+    &:disabled {
+      cursor: not-allowed;
+    }
+  }
+</style>
