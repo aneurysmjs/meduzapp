e8dccdd1b8486ca463237748a3fae1213435d2f0
aneurysm <aneurysmjs@outlook.com>
2022-06-23 00:56:17 +0200

    refactor(router): update router api's

--- src/app/App.routes.js
+++ src/app/App.routes.js
@@ -1,21 +1,26 @@
-import Vue from 'vue';
-import Router from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router'
+
 const Home = () =>  import('components/MHome/MHome');
 const MNewsArticle = () => import('components/MNewsArticle/MNewsArticle');
 
-Vue.use(Router);
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/:articleKey',
+    name: 'article',
+    component: MNewsArticle
+  }
+]
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/:articleKey',
-      name: 'article',
-      component: MNewsArticle
-    }
-  ]
-});
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+export default createRouter({
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  history: createWebHashHistory(),
+  routes, // short for `routes: routes`
+})
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './app/App'
-// import router from './app/App.routes';
+import router from './app/App.routes';
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app');
+
 
